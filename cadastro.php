4d3095321efb5fe75748d7b9d7c6c2e22054edf6
gustavo <ggdonato27@gmail.com>
2023-03-24 15:34:25 -0300

    criação do banco de dados, tela cadastro,telaregistro e conexão

---
+++ cadastro.php
@@ -0,0 +1,80 @@
+<?php
+    include("../atv_dashboard/dashboard-senalivre/header.php");
+    include("../atv_dashboard/conexao.php");
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+        if(!empty($_POST["cpf"])) {
+            $nome = $_POST["nome"];
+            $email = $_POST["email"];
+            $cpf = $_POST["cpf"];
+            $senha = $_POST["senha"];
+    
+            $sql ="INSERT INTO usuarios (nome, email, cpf, senha) VALUES (:nome, :email, :cpf, :senha) ";
+            $stmt = $conexao->prepare($sql);
+            $stmt->bindValue(":nome", $nome);
+            $stmt->bindValue(":email", $email);
+            $stmt->bindValue(":cpf", $cpf);
+            $stmt->bindValue(":senha", $senha);
+            $stmt->execute();
+    
+            if($stmt->rowCount() > 0){
+                echo"<div class='sucess'>usuario cadastrado com sucesso</div>";
+            }else{
+                echo"<div class='error'> falha ao registrar o usuario</div>";
+            }
+
+        } else {
+
+            $email = $_POST["email"];
+            $senha = $_POST["senha"];
+
+            $sql = "SELECT * FROM usuarios WHERE email = :email and senha = :senha";
+            $stmt = $conexao->prepare($sql);
+        
+            $stmt->bindValue(":email", $email);
+            $stmt->bindValue(":senha", $senha);
+            $stmt->execute();
+
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if($result){
+               header("Location: telalistagem.php");
+            }
+            else {
+                echo"<div class = 'error'>falha em logar o usuário</div>";
+            }
+        }
+
+       
+    }
+?>
+<div id="cad">
+        <nav id="cad-nav">
+            <a href="../atv_dashboard/dashboard-senalivre/index.php">Voltar</a>
+        </nav>
+    <input type="checkbox" id="chk" aria-hidden="true">
+    <div class="signup">
+        <form class="imp" method="POST">
+            <label for="chk" aria-hidden="true">Cadastre-se</label>
+            <input type="text" name="nome" id="nome" placeholder="Nome" required="">
+            <input type="number" name="cpf" id="cpf" placeholder="CPF" required="">
+            <input type="email" name="email" id="email" placeholder="Email" required="">
+            <input type="password" name="senha" id="senha" placeholder="Senha" required="">
+            <button class="cads">Sign up</button>
+        </form>
+</div>
+
+<div class="login">
+    <div class="menu">
+        <form class="imp" method="POST">
+            <label for="chk" aria-hidden="true">Login</label>
+            <input type="email" name="email" placeholder="Email" required="">
+            <input type="password" name="senha" placeholder="Senha" required="">
+            <button class="cads">Login</button>
+            </form>
+    </div>
+</div>
+
+<?php
+    include("../atv_dashboard/dashboard-senalivre/footer.php")
+?>
---
+++ dashboard-senalivre/index.php
@@ -0,0 +1,112 @@
+<?php
+    include("header.php");
+   ?>
+   
+   <menu>
+        <div class="logo">
+            <img src="../dashboard-senalivre/img/SENAI_São_Paulo_logo 1.png" alt="">
+        </div>
+        <nav class="navegando">
+            <ul>
+                <li>
+                    <a href="dash.html">
+                        <i class="fa-solid fa-ruler-horizontal"></i>
+                        Painel de controle
+                    </a>
+                </li>
+                <li>
+                    <a href="dash.html">
+                        <i class="fa-solid fa-users"></i>
+                        Usuario
+                    </a>
+                </li>
+                <li>
+                    <a href="dash.html">
+                        <i class="fa-solid fa-user"></i>
+                        Minha conta
+                    </a>
+                </li>
+                <li>
+                    <a href="dash.html">
+                        <i class="fa-solid fa-shield"></i>
+                        Segurança
+                    </a>
+                </li>
+                <li>
+                    <a href="dash.html">
+                        <i class="fa-regular fa-file"></i>
+                        Corporativo
+                    </a>
+                </li>
+                <li>
+                    <a href="dash.html">
+                        <i class="fa-solid fa-volleyball"></i>
+                        Esportes
+                    </a>
+                </li>
+                <li>
+                    <a href="dash.html">
+                        <i class="fa-solid fa-chalkboard-user"></i>
+                        Acadêmico
+                    </a>
+                </li>
+                <li>
+                    <a href="dash.html">
+                        <i class="fa-solid fa-chart-line"></i>
+                        Relatórios
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </menu>
+    <section>
+        <header>
+            <button>Dashboard</button>
+            <nav>
+                <a href="">Página</a>
+                <a href="">Aplicativos</a>
+                <a href="">Ajuda</a>
+            </nav>
+            <nav>
+                <a href=""><i class="fa-solid fa-magnifying-glass"></i></a>
+            </nav>
+            <a class="menu" href=""><i class="fa-solid fa-bars"></i></a>
+        </header>
+<!-- =====================================================================================
+==========================================================================================
+========================================================================================== -->
+
+        <main>
+            <h1>E-commerce | Painel de Controle</h1>
+
+            <section id="partedecima">
+                <div class="x"><p>!</p></div>
+                <div class="y"><p>!</p></div>
+                <div class="x"><p>!</p></div>
+                <div class="y"><p>!</p></div>                
+            </section>
+
+            <section id="primeira_sec">
+                    <div class="x">
+                        <p>!</p>
+                    </div>
+                    <div class="y">
+                        <p>!</p>
+                    </div>
+            </section>
+
+            <section id="segunda_sec">
+                <div class="y">
+                    <p>!</p>
+                </div>
+                <div class="x">
+                    <p>!</p>
+                </div>
+            </section>
+            
+        </main>
+    </section>
+
+<?php
+include("footer.php");
+?>
